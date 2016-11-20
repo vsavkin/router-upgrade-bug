709d9a8ce7ca778ab4996c5745644479ab6b42ab
Filipe Silva <filipematossilva@gmail.com>
2016-11-20 01:54:26 +0000

    add phonecat n2 app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+  selector: 'phonecat-app',
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,31 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent }     from './app.component';
+import { CheckmarkPipe }    from './core/checkmark/checkmark.pipe';
+import { Phone }            from './core/phone/phone.service';
+import { PhoneDetailComponent } from './phone-detail/phone-detail.component';
+import { PhoneListComponent }   from './phone-list/phone-list.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    PhoneListComponent,
+    CheckmarkPipe,
+    PhoneDetailComponent
+  ],
+  providers: [
+    Phone
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
