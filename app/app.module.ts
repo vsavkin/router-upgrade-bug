9dc74a6d02ed2b15a71ef5af8bf0c77356ae877d
vsavkin <avix1000@gmail.com>
2016-11-22 15:16:47 -0800

    fix the issue

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,32 @@ import { UpgradeModule } from '@angular/upgrade/static';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent }     from './app.component';
 import { CheckmarkPipe }    from './core/checkmark/checkmark.pipe';
 import { Phone }            from './core/phone/phone.service';
+
+import { Routes, RouterModule, UrlHandlingStrategy, UrlTree, ROUTER_INITIALIZER } from '@angular/router';
+import { RouterUpgradeInitializer } from '@angular/router/upgrade';
+import { APP_BASE_HREF, HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { PhoneDetailComponent } from './phone-detail/phone-detail.component';
 import { PhoneListComponent }   from './phone-list/phone-list.component';
+
+
+class Ng1Ng2UrlHandlingStrategy implements UrlHandlingStrategy {
+  shouldProcessUrl(url: UrlTree) {
+    return url.toString().startsWith('/phones')
+            || url.toString() === '/';
+  }
+  extract(url: UrlTree) { return url; }
+  merge(url: UrlTree, whole: UrlTree) { return url; }
+}
+
+const routes: Routes = [
+  { path: '', redirectTo: 'phones', pathMatch: 'full' },
+  { path: 'phones',          component: PhoneListComponent },
+  { path: 'phones/:phoneId', component: PhoneDetailComponent }
+];
 
 @NgModule({
   imports: [
@@ -17,7 +37,7 @@ import { PhoneListComponent }   from './phone-list/phone-list.component';
     UpgradeModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes, {enableTracing: true, useHash: true})
   ],
   declarations: [
     AppComponent,
@@ -26,6 +46,7 @@ import { PhoneListComponent }   from './phone-list/phone-list.component';
     PhoneDetailComponent
   ],
   providers: [
+    {provide: APP_BASE_HREF, useValue: '/!'},
     Phone
   ],
   bootstrap: [ AppComponent ]
--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { UpgradeModule } from '@angular/upgrade/static';
-import { Router } from '@angular/router';
+import { Router, RouterPreloader } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { initialRouterNavigation } from '@angular/router/upgrade';
+import { ApplicationRef } from '@angular/core';
 
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
   const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
   upgrade.bootstrap(document.documentElement, ['phonecatApp']);
-  setTimeout(() => {
-    platformRef.injector.get(Router).initialNavigation();
-  }, 0);
 });
